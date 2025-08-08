e23d3896c34c5e3db8adc16b17853ae9fcb301a9
Han-Bin Song <thdgksq@naver.com>
2025-08-08 18:22:23 +0900

    project init

---
+++ src/test/java/com/zinzza_song/login_practice/LoginPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zinzza_song.login_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
